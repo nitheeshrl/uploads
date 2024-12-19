a58fc7b9da23963d1d00fccb130da4ff8aa446cb
nitheeshrl <rlnitheesh@gmail.com>
2024-12-19 21:52:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,19 @@ async function generatePublicUrl() {
 server.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
-server.post("/upload", (req, res) => {
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, uniqueSuffix+file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+server.post("/upload", upload single("profileImage"),(req, res) => {
  // const { password } = req.body;
 //const { salt } = req.body;
  console.log(req.file)
